6f4bc51ef5f30bc3b21259a799a5255f738b774a
Maksim Stadler <maksimstadler1@gmail.com>
2018-06-15 10:34:35 -0400

    ICS4U Final Project

--- Tkinter Graph Embedding TEST.py
+++ Tkinter Graph Embedding TEST.py
@@ -1,22 +1,27 @@
 from tkinter import *
 from pandas import *
 import matplotlib
-import matplotlib.pyplot as plt
-from matplotlib.figure import Figure
 
 matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 
-incidentData = read_csv('airline-safety.csv')
+# incidentData = read_csv('airline-safety.csv')
+
+matplotlib.style.use('dark_background')
 
 tk = Tk()
 
-# f = Figure(figsize=(10, 10), dpi=100)
-# a = f.add_subplot(111)
-# a.plot([1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3, 4, 5, 6, 7, 8])
-#
-# canvas = FigureCanvasTkAgg(f)
-# canvas.show()
-# canvas.get_tk_widget().pack(side='TOP', fill='BOTH', expand=True)
+
+def matplotCanvas(self):
+    f = Figure(figsize=(5, 5), dpi=100)
+    a = f.add_subplot(111)
+    a.plot([1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3, 4, 5, 6, 7, 8])
+
+    canvas = FigureCanvasTkAgg(f, self)
+    canvas.show()
+    canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=True)
+
 
 tk.mainloop()
