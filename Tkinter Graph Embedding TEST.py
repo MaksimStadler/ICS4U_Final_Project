be89556d86c219768c3aa8b1b780e9e2bc5c99d1
MaksimStadler <maksimstadler1@gmail.com>
2018-06-17 17:24:24 -0400

    ICS4U Final Project

--- Tkinter Graph Embedding TEST.py
+++ Tkinter Graph Embedding TEST.py
@@ -1,27 +1,34 @@
 from tkinter import *
 from pandas import *
 import matplotlib
+import seaborn as sb
 
-matplotlib.use('TkAgg')
+# matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 
-# incidentData = read_csv('airline-safety.csv')
+# from matplotlib.figure import Figure
+# from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+
+incidentData = read_csv('airline-safety.csv')
 
 matplotlib.style.use('dark_background')
 
+incidentPlot = sb.lmplot(x='incidents_85_99', y='avail_seat_km_per_week', data=incidentData)
+plt.show()
 tk = Tk()
 
+tk.title('TEST')
 
-def matplotCanvas(self):
-    f = Figure(figsize=(5, 5), dpi=100)
-    a = f.add_subplot(111)
-    a.plot([1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3, 4, 5, 6, 7, 8])
+statisticButton = Button(text='Statistics')
+# statisticButton.config(height=2, width=56)
+# statisticButton.bind(canvasPlot)
+statisticButton.grid(columnspan=3, sticky='nsew')
 
-    canvas = FigureCanvasTkAgg(f, self)
-    canvas.show()
-    canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=True)
-
-
+# def canvasPlot():
+# f = Figure(figsize=(3, 3), dpi=100)
+# a = f.add_subplot(111)
+# a.plot([1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3, 4, 5, 6, 7, 8])
+# canvas = FigureCanvasTkAgg(f, tk)
+# canvas.show()
+# canvas.get_tk_widget().grid(row=0, column=0)
 tk.mainloop()
--- Tkinter TEST.py
+++ Tkinter TEST.py
@@ -1,4 +1,32 @@
 from tkinter import *
+from pandas import *
+import matplotlib
+import seaborn as sb
+
+# matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+
+# def canvasPlot():
+#     f = Figure(figsize=(3, 3), dpi=100)
+#     a = f.add_subplot(111)
+#     a.plot([1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3, 4, 5, 6, 7, 8])
+#     canvas = FigureCanvasTkAgg(f, tk)
+#     canvas.show()
+#     canvas.get_tk_widget().grid(row=0, column=3)
+#     tk.update()
+
+# matplotlib.style.use('dark_background')
+#
+# incidentData = read_csv('airline-safety.csv')
+#
+# incidentPlot = sb.lmplot(x='airline', y='avail_seat_km_per_week', data=incidentData)
+
+
+# def canvasPlot():
+#     plt.show()
+
 
 tk = Tk()
 
@@ -20,6 +48,7 @@ airlineDropDown3.grid(row=0, column=2, sticky='ew')
 
 statisticButton = Button(text='Statistics')
 # statisticButton.config(height=2, width=56)
+# statisticButton.bind(canvasPlot)
 statisticButton.grid(columnspan=3, sticky='nsew')
 
 tk.columnconfigure(0, weight=1)
